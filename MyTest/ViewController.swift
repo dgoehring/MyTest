9d0f5eeddac46f4badbdd5246969b79a2563b63b
dgoehring <don@dgoehring.net>
2017-09-26 06:30:46 -0500

    Initial Commit

---
+++ MyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTest
+//
+//  Created by Don Goehring on 9/26/17.
+//  Copyright © 2017 Goehring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
